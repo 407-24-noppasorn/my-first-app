47f528fd9f809a1c690063b98cea0f471f8fc5cf
407-24-noppasorn <54600@yupparaj.ac.th>
2026-06-26 11:28:01 +0700

    Create digital_profile.py

---
+++ digital_profile.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title("✨ My Digital Profile")
+
+st.header("🧩 แนะนำตัว")
+st.write("นางสาวนภสร วงค์นิธิกุล")
+st.write("ชื่อเล่น ใบเตย")
+st.write("เลขที่ 24 ม.4/7")
+
+st.header("🎯 เป้าหมายและสิ่งที่อยากทำในอนาคต")
+st.write("เป้าหมาย: **อยากเรียนด้านวิทย์คณิตหรือวิชาการ")
+st.write("สิ่งที่อยากเป็นในอนาคต: แพทย์(Doctor)")
