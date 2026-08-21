a243e51255a0e998f855133f4cfe232c1d07bcc5
407-24-noppasorn <54600@yupparaj.ac.th>
2026-08-21 11:57:07 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -7,22 +7,30 @@ if "ans1_val" not in st.session_state:
     st.session_state.ans1_val = ""
 if "ans2_val" not in st.session_state:
     st.session_state.ans2_val = ""
+if "ans3_val" not in st.session_state:
+    st.session_state.ans3_val = ""
+if "ans4_val" not in st.session_state:
+    st.session_state.ans4_val = ""
 
 
 def reset_game():
     st.session_state.ans1_val = "" # เคลียร์ค่าช่องข้อ 1
     st.session_state.ans2_val = "" # เคลียร์ค่าช่องข้อ 2
+    st.session_state.ans3_val = "" # เคลียร์ค่าช่องข้อ 3
+    st.session_state.ans4_val = "" # เคลียร์ค่าช่องข้อ 4
     st.session_state.start = time.time() # เริ่มเวลาใหม่
     st.session_state.is_ended = False # ปิด Dialog
  
 
 @st.dialog("📊 สรุปผลการเล่นเกม")
-def show_result_dialog(ans1, ans2):
+def show_result_dialog(ans1, ans2, ans3, ans4):
     st.balloons()
     score = 0
 
     u_ans1 = ans1.strip().lower()
     u_ans2 = ans2.strip().lower()
+    u_ans3 = ans3.strip().lower()
+    u_ans4 = ans4.strip().lower()
  
     if u_ans1 == "apple":
         st.success("✅ ข้อ 1: ถูกต้อง")
@@ -36,6 +44,20 @@ def show_result_dialog(ans1, ans2):
         score += 1
     else:
         st.error(f"❌ ข้อ 2: ยังไม่ถูกต้อง (คุณตอบ '{u_ans2}')")
+
+
+    if u_ans3 == "mango":
+        st.success("✅ ข้อ 3: ถูกต้อง")
+        score += 1
+    else:
+        st.error(f"❌ ข้อ 3: ยังไม่ถูกต้อง (คุณตอบ '{u_ans3}')")
+
+ 
+    if u_ans4 == "Pen":
+        st.success("✅ ข้อ 4: ถูกต้อง")
+        score += 1
+    else:
+        st.error(f"❌ ข้อ 4: ยังไม่ถูกต้อง (คุณตอบ '{u_ans4}')")
 
  
    
@@ -73,6 +95,17 @@ ans2 = st.text_input(
 st.session_state.ans1_val = ans1
 st.session_state.ans2_val = ans2
 
+ans3 = st.text_input(
+     "ข้อ 3: I love _a_ _o. 🥭",
+     value=st.session_state.ans3_val,
+)
+ans4 = st.text_input(
+     "ข้อ 4: I alway use p_ _ to write . 🖊️",
+     value=st.session_state.ans4_val,
+)
+st.session_state.ans1_val = ans3
+st.session_state.ans2_val = ans4
+
 
 if "start" in st.session_state and not st.session_state.get("is_ended", False):
     if st.button("ส่งคำตอบ"):
@@ -84,7 +117,7 @@ if "start" in st.session_state and not st.session_state.get("is_ended", False):
 
 
 if st.session_state.get("is_ended", False):
-    show_result_dialog(ans1, ans2)
+    show_result_dialog(ans1, ans2, ans3, ans4)
 
 st.divider()
 st.write("นางสาวนภสร วงค์นิธิกุล เลขที่ 24 ม.4/7")
